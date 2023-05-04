ba88225a799ee9d3c8b4d4098b39b0c6f6fb6911
mohdrehansiddiqui <Rehgraphicstudio08august@gmail.com>
2023-05-04 10:55:51 +0500

    3rd commit

--- public/app.js
+++ public/app.js
@@ -94,26 +94,17 @@ Fblogin.addEventListener('click',()=>{
   
   signInWithPopup(auth, provider)
   .then((result) => {
-    // The signed-in user info.
     const user = result.user;
-
-    // This gives you a Facebook Access Token. You can use it to access the Facebook API.
     const credential = FacebookAuthProvider.credentialFromResult(result);
     const accessToken = credential.accessToken;
     console.log(user,accessToken)
-
-    // IdP data available using getAdditionalUserInfo(result)
-    // ...
+    window.location.href = 'welcome.html'
   })
   .catch((error) => {
-    // Handle Errors here.
     const errorCode = error.code;
     const errorMessage = error.message;
-    // The email of the user's account used.
     const email = error.customData.email;
-    // The AuthCredential type that was used.
     const credential = FacebookAuthProvider.credentialFromError(error);
     console.log(errorCode,errorMessage,email,credential)
-    // ...
   });
 })
